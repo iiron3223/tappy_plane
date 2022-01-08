091851659399730fc6c851eefefa2bcb24232625
Dominik Baran <iiron3223@gmail.com>
2022-01-09 00:19:08 +0100

    feat: Add motion to a player

--- sprites.py
+++ sprites.py
@@ -3,7 +3,7 @@ import pygame as pg
 from main import Game
 from settings import *
 
-
+vector = pg.math.Vector2
 
 
 class Player(pg.sprite.Sprite):
@@ -14,7 +14,23 @@ class Player(pg.sprite.Sprite):
         self.image = pg.Surface((TILESIZE, TILESIZE))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
-        self.rect.bottomright = pos
+        self.pos = vector(pos)
+        self.rect.bottomright = self.pos
+        self.vel = vector(100, 0)
+        self.acc = vector(0, GRAVITY)
+        
+    def get_keys(self):
+        keys = pg.key.get_pressed()
+        if keys[pg.K_SPACE]:
+            self.flap()
         
     def update(self):
-        pass
+        self.get_keys()
+        self.vel += self.acc
+        self.vel += self.acc * self.game.dt
+        self.pos += self.vel * self.game.dt + 0.5 * self.acc * self.game.dt ** 2
+        self.rect.bottomright = self.pos
+
+    def flap(self):
+        """Apply upward force to player."""
+        self.vel.y = FLAP_POWER
